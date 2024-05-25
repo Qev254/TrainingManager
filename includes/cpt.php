a7a7c3a1df44b32f9a3f7970434227fca408fbe5
Qev254 <kelvinkkaranja@gmail.com>
2024-05-25 21:42:20 +0300

    Update cpt.php

--- includes/cpt.php
+++ includes/cpt.php
@@ -29,6 +29,42 @@ function tm_register_cpts() {
         'capability_type' => array('training_proposal', 'training_proposals'),
         'map_meta_cap' => true,
     );
+
+    // Inside tm_register_cpts()
+$proposal_args = array(
+    // ... other arguments ...
+    'capability_type' => array('training_proposal', 'training_proposals'),
+    'capabilities' => array(
+        'publish_posts' => 'publish_training_proposals',
+        'edit_posts' => 'edit_training_proposals',
+        'edit_others_posts' => 'edit_others_training_proposals',
+        'delete_posts' => 'delete_training_proposals',
+        'delete_others_posts' => 'delete_others_training_proposals',
+        'read_private_posts' => 'read_private_training_proposals',
+        'edit_post' => 'edit_training_proposal',
+        'delete_post' => 'delete_training_proposal',
+        'read_post' => 'read_training_proposal',
+    ),
+    'map_meta_cap' => true,
+);
+
+$event_args = array(
+    // ... other arguments ...
+    'capability_type' => array('training_event', 'training_events'),
+    'capabilities' => array(
+        'publish_posts' => 'publish_training_events',
+        'edit_posts' => 'edit_training_events',
+        'edit_others_posts' => 'edit_others_training_events',
+        'delete_posts' => 'delete_training_events',
+        'delete_others_posts' => 'delete_others_training_events',
+        'read_private_posts' => 'read_private_training_events',
+        'edit_post' => 'edit_training_event',
+        'delete_post' => 'delete_training_event',
+        'read_post' => 'read_training_event',
+    ),
+    'map_meta_cap' => true,
+);
+
     register_post_type('training_proposal', $proposal_args);
 
     // Register Training Event CPT
